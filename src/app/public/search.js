9ba418ebf3a02af8df26f1377ea3ac6aa6c48fcd
Andreas Rohrbach <andi@andi-pad>
2023-08-20 22:13:15 +0200

    complete basic functionality - still ugly tho

--- src/app/public/search.js
+++ src/app/public/search.js
@@ -29,22 +29,51 @@ class QueryBuffer {
   consumeQuery() {
     const query = this.queries.pop()
 
-    //   rem
-    this.queries.push(query)
-
-    //   this.list.removeChild(
-    //     this.list.children.item(this.list.children.length - 1)
-    //   )
+    this.list.removeChild(
+      this.list.children.item(this.list.children.length - 1)
+    )
 
     return query;
   }
 }
 
+function createDownloadFn(option) {
+  return async () => {
+    console.log('click download');
+    const res = await fetch(`./convert/link/${encodeURIComponent(btoa(option.url))}`)
+    const blob = await res.blob()
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    a.href = url
+    a.download = `${option.title}.mp3`
+    document.body.appendChild(a)
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
+}
+
+function addOptions(ul, options) {
+  options.map((option) => {
+    const views = String(option.views).padStart(10, ' ')
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.appendChild(document.createTextNode('download'))
+    btn.onclick = createDownloadFn(option)
+    li.appendChild(btn)
+    li.appendChild(document.createTextNode(
+      `${views} | ${option.title} (${option.timestamp}) | ${option.author.name}`
+    ))
+    ul.appendChild(li)
+  })
+}
+
 function app() {
-  const convertButton = document.querySelector('#convertButton');
+  const searchButton = document.querySelector('#searchButton');
   const compileButton = document.querySelector('#compileButton');
   const searchTextarea = document.querySelector('#searchTextarea');
   const queryList = document.querySelector('#queryList');
+  const optionsList = document.querySelector('#optionsList');
 
 
   const buffer = new QueryBuffer(queryList)
@@ -61,14 +90,15 @@ function app() {
     searchTextarea.value = ''
   }
 
-  convertButton.onclick = async () => {
-    console.log('click convert');
+  searchButton.onclick = async () => {
+    console.log('click search');
     const query = buffer.consumeQuery()
     console.log('query:', query);
     if (query && query.length) {
       const res = await fetch(`./search/query/${encodeURIComponent(btoa(query))}`)
       const json = await res.json()
       console.log(json);
+      addOptions(optionsList, json)
     }
   }
 }
